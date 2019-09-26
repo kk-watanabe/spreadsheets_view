88271705907ddbef3aecb4e5f10ff387bd42f3ad
kk-watanabe <kazuki.watanabe@bebit.com>
2019-09-26 19:08:00 +0900

    feat: Add Login

---
+++ src/views/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <input v-model="value" type="text" name="spreadSheetsId" placeholder="スプレットシートのIDを入力してください" />
+
+    <button @click="onClick">送信</button>
+    {{value}}
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Model, Prop } from "vue-property-decorator";
+
+@Component
+export default class Login extends Vue{
+  value: string = "";
+
+  onClick()  {
+    if (this.value.length > 0)    {
+      this.$store.dispatch("auth/login", this.value);
+      this.$store.dispatch("category/fetchCategoryClassInfo");
+    }
+  }
+}
+</script>
