dcdebffa6fe61ff5372848a2652851e7664d9ae5
kk-watanabe <kazuki.watanabe@bebit.com>
2019-09-26 17:55:45 +0900

    fix: Fixed api to be able to use category.

--- src/store/modules/api.ts
+++ src/store/modules/api.ts
@@ -1,7 +1,9 @@
 import HttpClient from "@/api/HttpClient";
+import ApiCategory from "@/api/ApiCategory";
 
 export class ApiState {
   httpClient = new HttpClient();
+  category = new ApiCategory(this.httpClient);
 }
 
 export const api = {
