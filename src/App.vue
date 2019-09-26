55ea986ad0924447e028742956f8a91fe0143ead
kk-watanabe <kazuki.watanabe@bebit.com>
2019-09-26 19:08:37 +0900

    refactor: Remove unnecessary elements.

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
@@ -13,9 +9,6 @@ import { Component, Vue } from "vue-property-decorator";
 
 @Component
 export default class App extends Vue{
-  mounted()  {
-    this.$store.dispatch("initialize");
-  }
 }
 </script>
 
