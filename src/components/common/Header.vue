0c150407226637fa2a0b6ef2863de173b06c040f
kk-watanabe <kazuki.watanabe@bebit.com>
2019-09-27 18:46:29 +0900

    feat: Add component to Header

---
+++ src/components/common/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header">
+    <div class="header__logo">Style guide views</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Header extends Vue{
+}
+</script>
+
+<style scoped lang="scss">
+</style>
