f4d5c818e97ac7b33b2c020287e77d3d27b1df1d
kk-watanabe <kazuki.watanabe@bebit.com>
2019-09-30 12:51:09 +0900

    feat: Add component to GlobalNavi

---
+++ src/views/GlobalNavi.vue
@@ -0,0 +1,25 @@
+<template>
+  <nav class="global-navi">
+    <div class="global-navi__wrapper">
+      <div v-for="category in categories" class="global-navi__item" :key="category.id">
+        <RouterLink :to="'/' + category.name">{{ category.name }}</RouterLink>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Category } from "@/models/Category";
+
+@Component
+export default class GlobalNavi extends Vue{
+  get categories(): Category[]  {
+    console.log("test")
+    return this.$store.state.category.categories;
+  }
+}
+</script>
+
+<style scoped lang="scss">
+</style>
