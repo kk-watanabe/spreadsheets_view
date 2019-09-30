14d546a749c5cf22c7a20501894912a1a4f3e75a
kk-watanabe <kazuki.watanabe@bebit.com>
2019-09-30 11:32:19 +0900

    feat: Add component to Footer

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Header from "@/views/Header.vue";
+import Footer from "@/components/common/Footer.vue";
 
 @Component({
   components: {
-    Header
+    Header,
+    Footer
   }
 })
 export default class App extends Vue{
---
+++ src/components/common/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="footer">
+    <a href="#container" class="footer__pagetop">ページTOP</a>
+    <small class="footer__copyright">© 2019 Style guide views.</small>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Footer extends Vue{
+}
+</script>
+
+<style scoped lang="scss">
+</style>
