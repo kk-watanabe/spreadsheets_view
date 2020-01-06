a2e890b45feae89a35fab19c0c307c38ee93979d
kk-watanabe <kazuki.watanabe@bebit.com>
2020-01-06 11:25:38 +0900

    feat: Add component to Button

--- src/components/atoms/Button.vue
+++ src/components/atoms/Button.vue
@@ -2,7 +2,7 @@
   <button
     type="button"
     class="button"
-    :class="[isColorClass, { 'button-disabled': disabled }]"
+    :class="[isColorClass, { 'button--disabled': disabled }]"
     @click="onClick"
   >
     <slot></slot>
@@ -12,22 +12,77 @@
 <script lang="ts">
 import { Component, Prop, Emit, Vue } from "vue-property-decorator";
 
+export enum buttonType {
+  Dark = "dark",
+  Light = "light",
+  Green = "green"
+}
+
 @Component
 export default class Button extends Vue {
-  @Prop({ type: String, default: "dark" })
+  @Prop({ type: String, default: buttonType.Dark })
   color!: String;
 
   @Prop({ type: Boolean, default: false })
   disabled!: boolean;
 
-  get isColorClass(): string {
-    return "button-" + this.color;
-  }
+  @Emit("click")
+  onClick(event: MouseEvent) {}
 
-  onClick(event: MouseEvent): void {
-    this.$emit("click", event);
+  get isColorClass(): string {
+    return "button--" + this.color;
   }
 }
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0 10px;
+  width: 100%;
+  height: 40px;
+  border: 1px solid;
+  border-radius: $border_radius;
+  transition: border-color $anime_base_duration ease,
+    background-color $anime_base_duration ease;
+
+  &--dark:not(.button--disabled) {
+    border-color: $is_color_gray900;
+    background-color: $is_color_gray900;
+    color: $is_color_white;
+
+    &:hover {
+      border-color: $is_color_gray800;
+      background-color: $is_color_gray800;
+    }
+  }
+
+  &--light:not(.button--disabled) {
+    border: 1px solid $is_color_gray800;
+    background-color: $is_color_white;
+    &:hover {
+      background-color: $is_color_gray100;
+    }
+  }
+
+  &--green:not(.button--disabled) {
+    border-color: $is_base_color200;
+    background-color: $is_base_color200;
+    color: $is_color_white;
+    &:hover {
+      border-color: $is_base_color300;
+      background-color: $is_base_color300;
+    }
+  }
+
+  &--disabled {
+    border-color: $is_color_gray400;
+    background-color: $is_color_gray400;
+    color: $is_color_white;
+    cursor: default;
+    pointer-events: none;
+  }
+}
+</style>
