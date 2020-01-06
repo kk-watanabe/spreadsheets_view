a7017e5a90984bd1fc1eb4a2ffc112208e9a1b3a
kk-watanabe <kazuki.watanabe@bebit.com>
2020-01-06 11:26:08 +0900

    feat: Add component to Footer

--- src/components/common/Footer.vue
+++ src/components/common/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer">
-    <a href="#container" class="footer__pagetop">ページTOP</a>
+    <a v-if="loggedIn" href="#container" class="footer__pagetop">ページTOP</a>
     <small class="footer__copyright">© 2019 Style guide views.</small>
   </div>
 </template>
@@ -9,7 +9,23 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 
 @Component
-export default class Footer extends Vue {}
+export default class Footer extends Vue {
+  @Prop({ type: Boolean, required: true })
+  loggedIn!: boolean;
+}
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: $footer_height;
+  background-color: $is_base_color100;
+  &__copyright {
+    color: $is_color_white;
+
+    @include font-size(11);
+  }
+}
+</style>
