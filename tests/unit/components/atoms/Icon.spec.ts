dccfa4026d287f75519aed00b2e4e67b7ab6f791
kk-watanabe <kazuki.watanabe@bebit.com>
2020-01-16 21:38:46 +0900

    feat: Add unit test to Icon

---
+++ tests/unit/components/atoms/Icon.spec.ts
@@ -0,0 +1,33 @@
+import { shallowMount } from "@vue/test-utils";
+import { Icons } from "@/const/Icons";
+import Icon from "@/components/atoms/Icon.vue";
+
+const factory = (values = {}) => {
+  return shallowMount(Icon, { ...values });
+};
+
+describe("Icon.vue", () => {
+  describe("Prop", () => {
+    // Icon propがある時 icon classが付与する
+    it("Has icon prop is add icon class", () => {
+      const wrapper = factory({
+        propsData: {
+          icon: Icons.Books
+        }
+      });
+      expect(wrapper.classes()).toContain(Icons.Books);
+    });
+    // Icons const で指定された class 全てが付与される
+    it("Has icon prop is add icon class to all Icons const", () => {
+      const icons = Object.entries(Icons);
+      icons.forEach(icon => {
+        const wrapper = factory({
+          propsData: {
+            icon: icon[1]
+          }
+        });
+        expect(wrapper.classes()).toContain(icon[1]);
+      });
+    });
+  });
+});
