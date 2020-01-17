020e2e53d7f627c2e8139c8a6a133eda44c19a61
kk-watanabe <kazuki.watanabe@bebit.com>
2020-01-17 19:57:54 +0900

    feat: Add story to IconButton

---
+++ src/stories/components/atoms/IconButton.stories.ts
@@ -0,0 +1,39 @@
+import { storiesOf } from "@storybook/vue";
+import { action } from "@storybook/addon-actions";
+import { number, boolean, select } from "@storybook/addon-knobs";
+import { ButtonType } from "@/const/Button";
+import { Icons } from "@/const/Icons";
+import IconButton from "@/components/atoms/IconButton.vue";
+
+storiesOf("Atom/IconButton", module).add(
+  "default",
+  () => ({
+    components: { IconButton },
+    props: {
+      icon: {
+        default: select("Icon", Icons, Icons.Home)
+      },
+      color: {
+        default: select("Color", ButtonType, ButtonType.Dark)
+      },
+      iconSize: {
+        default: number("Icon size", 26)
+      },
+      disabled: {
+        default: boolean("Disabled", false)
+      }
+    },
+    template: `<IconButton
+     :icon="icon"
+     :color="color"
+     :icon-size="iconSize"
+     :disabled="disabled"
+     @click="action" />`,
+    methods: { action: action("clicked") }
+  }),
+  {
+    info: {
+      summary: "Simple IconButton component"
+    }
+  }
+);
