1ce1cc653916be9ea5b5cdf6b49baf6ed1d2c1a5
kk-watanabe <kazuki.watanabe@bebit.com>
2020-01-20 21:03:18 +0900

    feat: Add component to StyleGuide

---
+++ src/views/StyleGuide.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="style-guide">style guide</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Model, Prop } from "vue-property-decorator";
+
+@Component
+export default class StyleGuide extends Vue {}
+</script>
