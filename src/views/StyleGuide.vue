a5db3d2cb937a4c602c2b98fab365d2e15e40167
kk-watanabe <kazuki.watanabe@bebit.com>
2020-01-22 16:54:01 +0900

    feat: Add created method to StyleGuide

--- src/views/StyleGuide.vue
+++ src/views/StyleGuide.vue
@@ -4,7 +4,27 @@
 
 <script lang="ts">
 import { Component, Vue, Model, Prop } from "vue-property-decorator";
+import { LoginInfo } from "@/models/Login";
 
 @Component
-export default class StyleGuide extends Vue {}
+export default class StyleGuide extends Vue {
+  get spreadSheetsId(): string {
+    return this.$store.state.auth.spreadSheetsId;
+  }
+
+  get spreadSheetsName(): string {
+    return this.$store.state.auth.spreadSheetsName;
+  }
+
+  created() {
+    if (this.spreadSheetsId) {
+      const loginInfo: LoginInfo = {
+        id: this.spreadSheetsId,
+        name: this.spreadSheetsName
+      };
+
+      this.$store.dispatch("auth/login", loginInfo);
+    }
+  }
+}
 </script>
