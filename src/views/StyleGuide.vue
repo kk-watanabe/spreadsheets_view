39735f3cbd5b301aa62b4a925d499e582bc5bd75
kk-watanabe <kazuki.watanabe@bebit.com>
2020-02-21 20:30:06 +0900

    fix: Delete unused ones

--- src/views/StyleGuide.vue
+++ src/views/StyleGuide.vue
@@ -8,23 +8,5 @@ import { LoginInfo } from "@/models/Login";
 import VueRouter from "vue-router";
 
 @Component
-export default class StyleGuide extends Vue {
-  get loggedIn(): boolean {
-    return this.$store.getters["auth/loggedIn"];
-  }
-
-  get spreadSheetsId(): string {
-    return this.$store.state.auth.spreadSheetsId;
-  }
-
-  get spreadSheetsName(): string {
-    return this.$store.state.auth.spreadSheetsName;
-  }
-
-  created() {
-    if (!this.loggedIn) {
-      this.$router.push("/login");
-    }
-  }
-}
+export default class StyleGuide extends Vue {}
 </script>
