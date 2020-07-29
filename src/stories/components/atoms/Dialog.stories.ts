24fe2d1b0b0b562605c960a5a8cc08168b9bd282
kk-watanabe <kazuki.watanabe@bebit.com>
2020-07-29 20:46:54 +0900

    fix: Fix Dialog because element-ui was deleted

--- src/stories/components/atoms/Dialog.stories.ts
+++ src/stories/components/atoms/Dialog.stories.ts
@@ -3,128 +3,55 @@ import { action } from "@storybook/addon-actions";
 import { text, boolean } from "@storybook/addon-knobs";
 import Dialog from "@/components/atoms/Dialog.vue";
 
-storiesOf("Atom/Dialog", module)
-  .add(
-    "default",
-    () => ({
-      components: { Dialog },
-      props: {
-        visible: {
-          default: boolean("Visible", true, "Dialog props"),
-        },
-        title: {
-          default: text("Title", "Title", "Dialog props"),
-        },
-        width: {
-          default: text("Width", "50%", "Dialog props"),
-        },
-        top: {
-          default: text("Top", "15vh", "Dialog props"),
-        },
-        appendToBody: {
-          default: boolean("Append To Body", true, "Dialog props"),
-        },
-        closeOnClickModal: {
-          default: boolean("Close On Click Modal", true, "Dialog props"),
-        },
-        showCloseButton: {
-          default: boolean("Show Close Button", true, "Dialog props"),
-        },
-        center: {
-          default: boolean("Center", true, "Dialog props"),
-        },
-        destroyOnClose: {
-          default: boolean("Destroy on close", true, "Dialog props"),
-        },
-        contentSlot: {
-          default: text("Content Slot", "Default slot"),
-        },
+storiesOf("atom/Dialog", module).add(
+  "default",
+  () => ({
+    components: { Dialog },
+    data() {
+      return {
+        value: false,
+      };
+    },
+    props: {
+      title: {
+        default: text("Title", "Title"),
       },
-      methods: { open: action("open"), close: action("close") },
-      template: `
-      <Dialog class="dialog"
-       :visible="visible"
-       :title="title"
-       :width="width"
-       :top="top"
-       :append-to-body="appendToBody"
-       :close-on-click-modal="closeOnClickModal"
-       :show-close-button="showCloseButton"
-       :center="center"
-       :destroy-on-close="destroyOnClose"
-       @open="open"
-       @close="close"
-      >
-        {{ contentSlot }}
-      </Dialog>`,
-    }),
-    {
-      info: {
-        summary: "Simple Dialog component",
+      width: {
+        default: text("Width", "50%"),
       },
-    }
-  )
-  .add(
-    "header and footer slot",
-    () => ({
-      components: { Dialog },
-      props: {
-        visible: {
-          default: boolean("Visible", true, "Dialog props"),
-        },
-        width: {
-          default: text("Width", "50%", "Dialog props"),
-        },
-        top: {
-          default: text("Top", "15vh", "Dialog props"),
-        },
-        appendToBody: {
-          default: boolean("Append To Body", true, "Dialog props"),
-        },
-        closeOnClickModal: {
-          default: boolean("Close On Click Modal", true, "Dialog props"),
-        },
-        showCloseButton: {
-          default: boolean("Show Close Button", true, "Dialog props"),
-        },
-        center: {
-          default: boolean("Center", true, "Dialog props"),
-        },
-        destroyOnClose: {
-          default: boolean("Destroy on close", true, "Dialog props"),
-        },
-        contentSlot: {
-          default: text("Content Slot", "Default slot"),
-        },
-        titleSlot: {
-          default: text("Title Slot", "Title slot"),
-        },
-        footerSlot: {
-          default: text("Footer Slot", "Footer slot"),
-        },
+      top: {
+        default: text("Top", "15vh"),
       },
-      methods: { open: action("open"), close: action("close") },
-      template: `
-      <Dialog class="dialog"
-       :visible="visible"
-       :width="width"
-       :top="top"
-       :append-to-body="appendToBody"
-       :close-on-click-modal="closeOnClickModal"
-       :show-close-button="showCloseButton"
-       :center="center"
-       :destroy-on-close="destroyOnClose"
-       @open="open"
-       @close="close"
-      >
-        <div slot="title">{{ titleSlot }}</div>
-        {{ contentSlot }}
-        <div slot="footer">{{ footerSlot }}</div>
-      </Dialog>`,
-    }),
-    {
-      info: {
-        summary: "header and footer slot",
+      isCenter: {
+        default: boolean("isCenter", false),
       },
-    }
-  );
+    },
+    methods: {
+      onOpen: action("open"),
+      onClose: action("close"),
+    },
+    template: `
+      <div>
+        <label><input type="checkbox" v-model="value" />Open Dialog</label>
+
+        <Dialog
+         v-model="value"
+         :width="width"
+         :top="top"
+         :title="title"
+         :is-center="isCenter"
+         @open="onOpen"
+         @close="onClose"
+        >
+          <template v-slot:header>header text slot</template>
+          <div>content text slot</div>
+          <template v-slot:footer>footer text slot</template>
+        </Dialog>
+      </div>`,
+  }),
+  {
+    info: {
+      summary: "Simple Dialog component",
+    },
+  }
+);
