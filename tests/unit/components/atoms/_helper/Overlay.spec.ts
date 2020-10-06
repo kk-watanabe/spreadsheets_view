714ccb310169c54ba264e495f66d0147c6da775f
kk-watanabe <kazuki.watanabe@bebit.com>
2020-10-06 16:28:13 +0900

    feat: Add unit test to Overlay

---
+++ tests/unit/components/atoms/_helper/Overlay.spec.ts
@@ -0,0 +1,42 @@
+import { shallowMount } from "@vue/test-utils";
+import "@testing-library/jest-dom";
+import { checkOneCalledEmitted, transitionStub } from "@test-utils/test-util";
+import Overlay from "@/components/atoms/_helper/Overlay.vue";
+
+const factory = (values = {}) => {
+  return shallowMount(Overlay, {
+    stubs: {
+      Transition: transitionStub(),
+    },
+    propsData: {
+      value: true,
+      ...values,
+    },
+  });
+};
+
+describe("Overlay.vue", () => {
+  describe("Prop", () => {
+    it("Show Overlay when value prop is true", () => {
+      const wrapper = factory();
+
+      expect(wrapper.element).toBeVisible();
+    });
+
+    it("Hidden Overlay when value prop is false", () => {
+      const wrapper = factory({
+        value: false,
+      });
+
+      expect(wrapper.element).not.toBeVisible();
+    });
+
+    it("Add style zIndex when zIndex prop is 4000", () => {
+      const wrapper = factory({
+        zIndex: 4000,
+      });
+
+      expect(wrapper.attributes().style).toBe("z-index: 4000;");
+    });
+  });
+});
