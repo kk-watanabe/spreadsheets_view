01cb9ec40256f84bc1a31eeeac28c49b3960536c
kk-watanabe <kazuki.watanabe@bebit.com>
2020-10-15 20:52:11 +0900

    feat: Add assets folder

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -119,32 +119,14 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from "vue-class-component";
-
-@Options({
+export default {
   props: {
-    msg: String,
+    msg: {
+      type: String,
+      required: true,
+    },
   },
-})
-export default class HelloWorld extends Vue {
-  msg!: string;
-}
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="scss">
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<style scoped lang="scss"></style>
